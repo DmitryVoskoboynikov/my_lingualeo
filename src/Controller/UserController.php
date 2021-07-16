09f93047573b4a0aafbd35aafd6c80582598e5d7
Dmitry Voskoboynikov <dmitry.voskoboynikoff@gmail.com>
2021-07-16 10:22:49 +0300

    portion of code

---
+++ src/Controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+// src/Controller/UserController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user/notifications", name="user_notifications")
+     */
+    public function notifications(): Response
+    {
+        // get the user information and notifications somehow
+        $userFirstName = 'Dmitry';
+        $userNotifications = ['one', 'two'];
+
+        // the template path is the relative file path from `templates/`
+        return $this->render('user/notifications.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'user_first_name' => $userFirstName,
+            'notifications' => $userNotifications,
+        ]);
+    }
+}
