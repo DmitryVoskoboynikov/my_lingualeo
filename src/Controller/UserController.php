c2e3917b7531c93d1d135de631e1d542b8ad6df5
Dmitry Voskoboynikoff <dmitry.voskoboynikoff@gmail.com>
2023-05-16 18:25:27 +0300

    create of user registration

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,32 @@
 // src/Controller/UserController.php
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Text;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormError;
+
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class UserController extends AbstractController
 {
-    // ...
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
 
     public function notifications(): Response
     {
@@ -29,7 +46,7 @@ class UserController extends AbstractController
         ]);
     }
 
-    public function auth(Request $request): Response
+    public function auth(Request $request, UserRepository $rep): Response
     {
         $form = $this->createFormBuilder(null, [
                 'action' => '/auth',
@@ -45,13 +62,49 @@ class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
 
-            // ... perform some action, such as saving the data to the database
+            $user = $rep->findOneBy(['email' => $data['email']]);
+            if ($user && ($user->getPassword() == $data['password'])) {
+                return $this->redirectToRoute('user_notification');
+            } else {
+                $form->addError(
+                    new FormError('Пользователя с такой парой логин пароль не существует.')
+                );
+            }
 
-            return $this->redirectToRoute('user_notification');
         }
 
         return $this->render('user/auth.html.twig', [
             'form' => $form->createView(),
         ]);
     }
+
+    public function reg(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $user->getPassword()
+            );
+            $user->setPassword($hashedPassword);
+
+            $user->setRoles(['ROLE_USER']);
+
+            // Save
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_notification');
+        }
+
+        return $this->render('user/reg.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
